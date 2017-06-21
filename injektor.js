04d355450e52a62be9cc53388d6e698570e0bae6
Oliver Cooper <oliver.cooper@me.com>
2017-06-22 08:43:57 +1200

    Started cover animation

--- injektor.js
+++ injektor.js
@@ -8,8 +8,14 @@ $(document).ready(function() {
             url: 'https://open.spotify.com',
             name: 'wp_access_token'
         }, function(error, cookies) {
-            accessToken = cookies[0].value;
-            launch();
+            if (cookies[0]) {
+                accessToken = cookies[0].value;
+                launch();
+            }
+            else {
+                console.error("Cookie not found");
+                $spotifyWebView.removeClass("spotify-driver");
+            }
         });
     });
 });
@@ -27,10 +33,42 @@ function launch() {
         var $albums = $(".albums").html("");
         for (var i = 0; i < albums.length; i++) {
             var image = albums[i].images[0].url;
-            var $album = $(`<div class="album" style="background-image:url(${image})"></div>`);
+            var id = albums[i].id;
+            var $album = $(`<div class="album" id="${id}" style="background-image:url(${image})"></div>`).click(function() {
+                openAlbum(this.id);
+            });
             $albums.append($album);
         }
     });
+}
+
+function albumByID(id) {
+    for (var i = 0; i < albums.length; i++) {
+        if (albums[i].id == id)
+            return albums[i];
+    }
+}
+
+function openAlbum(id) {
+    // animate $album's cover moving up to the full position (although this is actually done backwards)
+    var album = albumByID(id);
+    var $album = $(".album#" + id);
+    $(".page-albums").addClass("out");
+    $(".page-playing").addClass("in");
+
+    var $cover = $(".cover");
+    var offset = $album.offset();
+
+    var x = (offset.left - $album.index() % 2 * $album.width()) * 2;
+    var y = (offset.top - window.scrollY - $album.height()) * 2;
+    $cover.css("transform", `scale(0.5) translate(${x}px, ${y}px)`).css("background-image", "url(" + album.images[0].url + ")");
+    $cover.addClass($album.index() % 2 == 0 ? "left" : "right");
+    $album.addClass("hide");
+    setTimeout(function() {
+        $cover.addClass("animate");
+        $cover.css("transform", "");
+    }, 20);
+
 }
 
 function request(path, data, success) {
